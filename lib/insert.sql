37e03bb921c942177e40c6e31a6157e6afc4e85a
Aaron Kelton <aaron.k.kelton@gmail.com>
2017-05-13 17:43:04 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,53 @@
+INSERT INTO series (title) VALUES
+  (""),
+  ("")
+;
+
+INSERT INTO subgenres (name) VALUES
+  (""),
+  ("")
+;
+
+INSERT INTO authors (name) VALUES
+  (""),
+  ("")
+;
+
+INSERT INTO books (title) VALUES
+  (""),
+  (""),
+  (""),
+  (""),
+  (""),
+  ("")
+;
+
+INSERT INTO characters (name) VALUES
+  (""),
+  (""),
+  (""),
+  (""),
+  (""),
+  (""),
+  (""),
+  ("")
+;
+
+INSERT INTO character_books (character_id, book_id) VALUES
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", ""),
+  ("", "")
+;
---
+++ lib/schema.sql
@@ -0,0 +1,49 @@
+-- Characters, Books, Series, Authors, and Sub-Genres
+
+CREATE TABLE IF NOT EXISTS characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+)
+;
+
+CREATE TABLE IF NOT EXISTS books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year TEXT,
+  series_id INTEGER
+)
+;
+
+CREATE TABLE IF NOT EXISTS series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
+)
+;
+
+CREATE TABLE IF NOT EXISTS authors (
+  id INTEGER PRIMARY KEY,
+  -- series_id INTEGER,
+  name TEXT
+  -- character_id INTEGER
+)
+;
+
+CREATE TABLE IF NOT EXISTS subgenres (
+  id INTEGER PRIMARY KEY,
+  -- series_id INTEGER,
+  name TEXT
+)
+;
+
+CREATE TABLE IF NOT EXISTS character_books (
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER,
+  book_id INTEGER
+)
+;
